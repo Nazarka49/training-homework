c0b79f65b4e5d8bc86737c37f2a921dc1b8d8e6b
unknown <nazarka49@gmail.com>
2024-04-16 00:49:38 +0200

    Lesson 3 - SQL task solution

--- lesson_02/job2/bll/json_to_avro.py
+++
@@ -1,7 +0,0 @@
-from lesson_02.job2.dal import json_to_avro
-
-
-def convert_json_to_avro(raw_dir: str, stg_dir: str) -> None:
-    print("\tI'm in get_sales(...) function!")
-    data = json_to_avro.get_json(raw_dir)
-    json_to_avro.save_avro(data, stg_dir)
