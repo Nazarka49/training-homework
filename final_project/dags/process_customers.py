a85f319d4ad13c756b254e4f1d0864a68e6f9be0
unknown <nazarka49@gmail.com>
2024-06-21 00:40:53 +0200

    part 2 commit

--- final_project/dags/process_customers.py
+++ final_project/dags/process_customers.py
@@ -10,8 +10,7 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExte
 
 DATA_LAKE_RAW_BUCKET = 'data_eng-hw_final_project_raw_data_nazar_meliukh'
 DATASET_NAME = 'de-07-nazar-meliukh'
-BUCKET_FILE_NAME = 'sales/{{ dag_run.logical_date.strftime("%Y-%m-%-d") }}/{{ dag_run.logical_date.strftime("%Y-%m-%-d") }}__sales.csv'
-
+BUCKET_FILE_NAME = 'customers/{{ dag_run.logical_date.strftime("%Y-%m-%-d") }}/*.csv'
 DEFAULT_ARGS = {
     'depends_on_past': True,
     'email_on_failure': True,
@@ -21,26 +20,28 @@ DEFAULT_ARGS = {
 }
 
 with DAG(
-        dag_id="sales_pipeline",
-        description="Ingest and process sales data",
+        dag_id="customers_pipeline",
+        description="Ingest and process customers data",
         schedule_interval='0 7 * * *',
-        start_date=dt.datetime(2022, 9, 1),
-        end_date=dt.datetime(2022, 9, 7),
+        start_date=dt.datetime(2022, 8, 1),
+        end_date=dt.datetime(2022, 8, 7),
         catchup=True,
-        tags=['sales'],
+        tags=['customers'],
         default_args=DEFAULT_ARGS,
 ) as dag:
 
     create_external_table_task = BigQueryCreateExternalTableOperator(
         task_id='create_external_table',
-        destination_project_dataset_table=f'{DATASET_NAME}.bronze.sales',
+        destination_project_dataset_table=f'{DATASET_NAME}.bronze.customers',
         bucket=DATA_LAKE_RAW_BUCKET,
         source_objects=[BUCKET_FILE_NAME],
         schema_fields=[
-            {"name": "CustomerId", "type": "STRING", "mode": "NULLABLE"},
-            {"name": "PurchaseDate", "type": "STRING", "mode": "NULLABLE"},
-            {"name": "Product", "type": "STRING", "mode": "NULLABLE"},
-            {"name": "Price", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "Id", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "FirstName", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "LastName", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "Email", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "RegistrationDate", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "State", "type": "STRING", "mode": "NULLABLE"},
         ],
         skip_leading_rows=1,
     )
@@ -51,7 +52,7 @@ with DAG(
         project_id=DATASET_NAME,
         configuration={
             "query": {
-                "query": "{% include 'sql/transfer_from_dwh_bronze_to_dwh_silver.sql' %}",
+                "query": "{% include 'sql/customers_dwh_bronze_to_dwh_silver.sql' %}",
                 "useLegacySql": False,
             }
         },
@@ -62,6 +63,6 @@ with DAG(
 
     delete_bronze_table_task = BigQueryDeleteTableOperator(
         task_id="delete_bronze_table",
-        deletion_dataset_table=f'{DATASET_NAME}.bronze.sales',
+        deletion_dataset_table=f'{DATASET_NAME}.bronze.customers',
     )
     create_external_table_task >> transfer_from_dwh_bronze_to_dwh_silver >> delete_bronze_table_task
